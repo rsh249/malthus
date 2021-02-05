043adeb817a646bd71364fc4d66d8210adc7e5d3
robert harbert <rharbert@stonehill.edu>
2021-02-04 21:53:48 -0500

    bash av data miner

---
+++ bash/av_test.sh
@@ -0,0 +1,20 @@
+# bash code to mine the Alpha Vantage API for long term daily stock market data
+#more notes and demo code: http://cafim.sssup.it/~giulio/other/alpha_vantage/index.html
+#module load parallel  #load gnu parallel or otherwise set up access
+source ./av_keys # get Alpha Vantage keys from key file or set environmental variables AV_KEY and AV_STUDENT KEY
+mkdir avdata
+
+# get list of NYSE symbols and metadata
+wget https://datahub.io/core/nyse-other-listings/r/nyse-listed.csv #may need updating this list is ~2 years old
+#wget 'http://old.nasdaq.com/screening/companies-by-name.aspx?letter=0&exchange=nyse&render=download' #investigate
+
+
+# collect symbol codes from nyse-listed.csv and wget the API url with an active API key:
+cat nyse-listed.csv | sed "1d" | awk -F ',' '{print $1}'  | 
+xargs -P 4 -n 1 -I {} wget --output-document "avdata/"{}".csv" "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&outputsize=full&apikey=$AV_KEY&datatype=csv&symbol="{}
+
+#put avdata in the hadoop file system 
+#assumes hadoop configured and accessible via hdfs
+hdfs dfs -put avdata/* avdata
+
+
---
+++ bash/nyc_taxi.sh
@@ -0,0 +1,14 @@
+# build data lake of NYC taxi data from:
+
+mkdir NYCtaxi
+
+# get data
+
+# put in hdfs
+
+
+# build hive table
+
+
+# example query
+
