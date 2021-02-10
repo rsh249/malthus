a8f8c5e0060693d2e3ae3bfe48cb3fc8084a3aa9
robert harbert <rharbert@stonehill.edu>
2021-02-10 00:40:35 -0500

    update some spark stuff

--- spark_ex/spark_avdata.scala
+++ spark_ex/spark_avdata.scala
@@ -1,8 +1,32 @@
 # Spark example
+
+#NOTES:
 #module load spark
 #export SPARK_WORKER_CORES=6
 #spark-shell # run to start scala spark interactive shell
-val nysedata = spark.read.textFile("file:/projectnb2/ct-shbioinf/rharbert/malthus/avdata/*")
-val df = nysedata.toDF("line")
-val j1 = df.filter(col("line").like("01/01/2018"))
-j1.first()
+#spark-shell --master local[6]
+
+
+###################################
+#set up spark context and configuration
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkConf
+import org.apache.spark.sql.functions.input_file_name
+
+
+val conf = new SparkConf().setMaster("local[6]").setAppName("CountingSheep")
+val sc = new SparkContext(conf)
+
+###################################
+
+//val scnyse = sc.textFile("file:/projectnb2/ct-shbioinf/rharbert/malthus/avdata/*").toDF()
+val scnyse = spark.read.format("csv").option("header", "true").load("file:/projectnb2/ct-shbioinf/rharbert/malthus/avdata/*")
+scnyse.count()
+
+
+// parse stock symbol from path to files
+val nextdf = scnyse.withColumn("path", input_file_name)
+nextdf.filter(scnyse("open") <= "10").show(false) //Filter for rows with open value <= $10
+
+
+
--- spark_ex/spark_nyctaxi.scala
+++ spark_ex/spark_nyctaxi.scala
@@ -2,7 +2,21 @@
 #module load spark
 #export SPARK_WORKER_CORES=6
 #spark-shell # run to start scala spark interactive shell
-val taxidata = spark.read.textFile("file:/projectnb2/ct-shbioinf/rharbert/malthus/nyctaxi/*")
+
+###################################
+#set up spark context and configuration
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkConf
+
+val conf = new SparkConf().setMaster("local[6]").setAppName("CountingSheep")
+val sc = new SparkContext(conf) //don't need to run this every time. Just once per user session?
+
+###################################
+
+
+val taxidata = spark.read.textFile("file:/projectnb2/ct-shbioinf/rharbert/malthus/nyctaxi/trip data/*")
 val df = taxidata.toDF("line")
 val j1 = df.filter(col("line").like("01/01/2018"))
 j1.first()
+
+val taxidf = sc.textFile("file:/projectnb2/ct-shbioinf/rharbert/malthus/nyctaxi/trip data/*").toDF()
