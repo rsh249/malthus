2a4bf54edcaeed0ba88a7df2be695bd84d1cd8db
robert harbert <rharbert@stonehill.edu>
2021-02-12 01:31:27 -0500

    lalasparkland

--- spark_ex/spark_avdata.scala
+++ spark_ex/spark_avdata.scala
@@ -1,32 +1,55 @@
-# Spark example
+// Spark example: NYSE data from AlphaVantage
 
-#NOTES:
-#module load spark
-#export SPARK_WORKER_CORES=6
-#spark-shell # run to start scala spark interactive shell
-#spark-shell --master local[6]
+//NOTES:
+//module load spark
+//export SPARK_WORKER_CORES=6
+//spark-shell # run to start scala spark interactive shell
+//spark-shell --master local[6]
 
 
-###################################
-#set up spark context and configuration
+// IMPORT ###################################
+//#set up spark context and configuration
 import org.apache.spark.SparkContext
 import org.apache.spark.SparkConf
 import org.apache.spark.sql.functions.input_file_name
+import spark.sql
 
 
-val conf = new SparkConf().setMaster("local[6]").setAppName("CountingSheep")
-val sc = new SparkContext(conf)
+val conf = new SparkConf().setMaster("local[6]").setAppName("nyse")
+// val sc = new SparkContext(conf) //spark context probably exists already
 
-###################################
+//###################################
 
+
+//read
 //val scnyse = sc.textFile("file:/projectnb2/ct-shbioinf/rharbert/malthus/avdata/*").toDF()
 val scnyse = spark.read.format("csv").option("header", "true").load("file:/projectnb2/ct-shbioinf/rharbert/malthus/avdata/*")
 scnyse.count()
 
 
 // parse stock symbol from path to files
-val nextdf = scnyse.withColumn("path", input_file_name)
-nextdf.filter(scnyse("open") <= "10").show(false) //Filter for rows with open value <= $10
+val df = scnyse.withColumn("path", input_file_name)
+df.show(false)
 
 
+//Can we trim the path?
+val df2 = df.withColumn("split_path", split($"path", "/")).drop("path")
+val df3 = df2.selectExpr("split_path[6]", "timestamp", "open", "high", "low", "close", "adjusted_close", "volume", "dividend_amount", "split_coefficient")
+df3.show(false) // show the contents of the data frame 
 
+// experiment with filtering
+val filter1 = df3.filter(col("open") <= "10") //Filter for rows with open value <= $10
+filter1.show(false)
+filter1.count() // 156000 rows (days observed per symbol) vs 9,000,000+ for all avdata
+
+val filter2 = df3.filter(col("close") > col("open")) // days when close > open
+filter2.show()
+filter2.count()
+
+
+// write output of filter1 to new csv file
+filter1.coalesce(1).write.csv("spark_nyse_filter") // writes output to single file in folder spark_nyse_filter
+
+
+// quit spark-shell
+:q
--- spark_ex/spark_nyctaxi.scala
+++ spark_ex/spark_nyctaxi.scala
@@ -4,19 +4,52 @@
 #spark-shell # run to start scala spark interactive shell
 
 ###################################
-#set up spark context and configuration
 import org.apache.spark.SparkContext
 import org.apache.spark.SparkConf
+import org.apache.spark.sql.functions.input_file_name
+import spark.sql
 
-val conf = new SparkConf().setMaster("local[6]").setAppName("CountingSheep")
-val sc = new SparkContext(conf) //don't need to run this every time. Just once per user session?
+
+val conf = new SparkConf().setMaster("local[6]").setAppName("taxidata")
+// val sc = new SparkContext(conf) //spark context probably exists already
 
 ###################################
 
 
-val taxidata = spark.read.textFile("file:/projectnb2/ct-shbioinf/rharbert/malthus/nyctaxi/trip data/*")
-val df = taxidata.toDF("line")
-val j1 = df.filter(col("line").like("01/01/2018"))
-j1.first()
+val taxidata = spark.read.format("csv").option("header", "true").load("file:/projectnb2/ct-shbioinf/rharbert/malthus/nyctaxi/trip data/*")
+taxidata.count()
 
-val taxidf = sc.textFile("file:/projectnb2/ct-shbioinf/rharbert/malthus/nyctaxi/trip data/*").toDF()
+// FIGURE OUT HOW TO REMOVE SPACES FROM COLUMN NAMES!!
+var newDf = taxidata 
+for(col <- taxidata.columns){ newDf = newDf.withColumnRenamed(col, col.replaceAll("\\s", "")) }
+val taxidata = newDf
+
+
+// get path to files
+val df = taxidata.withColumn("path", input_file_name)
+df.show(false)
+
+
+//Can we trim the path?
+val df2 = df.withColumn("split_path", split($"path", "/")).drop("path")
+
+val df3 = df2.selectExpr("split_path[7]", "fare_amount")
+df3.show(false) // show the contents of the data frame 
+
+// experiment with filtering (directly from taxidata)
+
+val filter1 = taxidata.filter(col(" fare_amount") >= "1000") 
+filter1.show(false)
+filter1.count() 
+
+val filter2 = taxidata.filter(col("trip_distance") > 100) 
+filter2.show()
+filter2.count()
+
+
+// write output of filter1 to new csv file
+filter1.coalesce(1).write.csv("spark_nyse_filter") // writes output to single file in folder spark_nyse_filter
+
+
+// quit spark-shell
+:q
