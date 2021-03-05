a13dd1c73eda6762203ecbd1a67d686e6456b39a
robert harbert <rharbert@stonehill.edu>
2021-03-05 16:36:02 -0500

    update pyspark version for taxi data analysis

---
+++ spark_ex/spark_nyctaxi.0.2.py
@@ -0,0 +1,44 @@
+#pyspark code for avdata exploration
+# append: 'export PYSPARK_PYTHON=python3' to .bashrc
+
+from pyspark.sql import SparkSession
+import pyspark.sql.functions as fun
+from pyspark.sql.types import StructType, StructField, StringType, DoubleType, IntegerType
+
+
+spark = SparkSession.builder \
+    .master("local[6]") \
+    .appName("avdata") \
+    .getOrCreate()
+    
+# warning: It is a little slower to read the data with inferSchema='True'. 
+#Need to define schema for NYC taxi data.
+schema = StructType([ \
+    StructField("pickup_datetime",StringType(),True), \
+    StructField("dropoff_datetime",StringType(),True), \
+    StructField("pu_loc_id",IntegerType(),True), \
+    StructField("do_loc_id", IntegerType(), True), \
+    StructField("passenger_count", IntegerType(), True), \
+    StructField("trip_distance", DoubleType(), True), \
+    StructField("fare_amount", StringType(), True), \
+    StructField("tip_amount", StringType(), True), \
+    StructField("total_amount", StringType(), True), \
+    StructField("last_dropoff_datetime", StringType(), True), \
+    StructField("last_do_loc_id", IntegerType(), True), \
+    
+  ])
+  
+df = spark.read.format("csv").options(header='True').schema(schema).load("nyctaxi/*")
+df.printSchema()
+
+#get a count of rows
+df.count()
+
+# get sourcefile name from input_file_name()
+df2 = df.withColumn("path", fun.input_file_name())
+regex_str = "[\/]([^\/]+[^\/]+)$" #regex to extract text after the last / or \
+df2 = df2.withColumn("sourcefile", fun.regexp_extract("path",regex_str,1))
+df2.show()
+
+#quit pyspark
+quit()
