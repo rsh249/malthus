4f5a24179948dd09209856d20964236f4ee55d90
robert harbert <rharbert@stonehill.edu>
2021-05-05 22:41:48 -0400

    spark clustering nyc taxi data

--- spark_ex/spark_nyctaxi.0.6.py
+++ spark_ex/spark_nyctaxi.0.6.py
@@ -5,6 +5,11 @@ from pyspark.sql import SparkSession
 import pyspark.sql.functions as fun
 from pyspark.sql.types import StructType, StructField, StringType, FloatType, DoubleType, IntegerType
 
+from pyspark.ml.clustering import KMeans
+from pyspark.ml.evaluation import ClusteringEvaluator
+from pyspark.ml import Pipeline
+from pyspark.ml.feature import VectorAssembler
+
 # set up the SparkSession
 spark = SparkSession.builder \
   .master("local") \
@@ -47,6 +52,34 @@ df=df.withColumn('pickup_hour', fun.hour("pickup_time"))
 df=df.withColumn('pickup_month', fun.month("pickup_time"))
 df.select('pickup_hour').summary().show()
 
-# clustering example on pickup_hour and loc_id
+# clustering example
+
+#set up dataset object with features
+pred_col = ["pickup_hour", "pickup_month", "DOLocationID", "PULocationID", "trip_distance"]
+dffeat = df.na.drop()
+vector_assembler = VectorAssembler(inputCols=pred_col, outputCol='features') #Create pipeline and pass it to stages
+pipeline = Pipeline(stages=[
+           vector_assembler
+])
+df_transformed = pipeline.fit(dffeat).transform(dffeat)
+
+# Trains a k-means model.
+kmeans = KMeans().setK(4).setSeed(1)
+model = kmeans.fit(df_transformed)
+
+# Make predictions
+predictions = model.transform(df_transformed)
+
+# Evaluate clustering by computing Silhouette score
+evaluator = ClusteringEvaluator()
+
+silhouette = evaluator.evaluate(predictions)
+print("Silhouette with squared euclidean distance = " + str(silhouette))
+
+# Shows the result.
+centers = model.clusterCenters()
+print("Cluster Centers: ")
+for center in centers:
+    print(center)
 
 
